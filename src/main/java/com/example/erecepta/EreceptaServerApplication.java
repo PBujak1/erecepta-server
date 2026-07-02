5f5454e5a80791bf5b93764cb948b642436a9d24
Piotr <1piotrbujak@gmail.com>
2026-07-02 14:55:10 +0200

    Initial commit

---
+++ src/main/java/com/example/erecepta/EreceptaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.erecepta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EreceptaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EreceptaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/erecepta/EreceptaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erecepta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EreceptaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
